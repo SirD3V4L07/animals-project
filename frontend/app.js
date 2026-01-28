07df1c06099be6052cdf773e9912039efc8fb376
Animalsdev <ascendantspiral@gmail.com>
2026-01-28 12:17:44 +0000

    Add html table with animal data and favicon file

--- frontend/app.js
+++ frontend/app.js
@@ -4,8 +4,20 @@ fetch("/api/health")
     console.log("From API:", data);
   });
 
-  fetch("/api/animals")
-  .then(res => res.json())
-  .then(data => {
-    console.log("From API:", data);
-  });
+  
+async function aniBuildTable() {
+  let table = document.getElementById("animal-table");
+  const aniAnimalList = await (await fetch("/api/animals")).json();
+  for (let index = 0; index < aniAnimalList.length; index++) {
+    let row = `
+      <tr>
+        <td>${aniAnimalList[index].common_name}</td>
+        <td>${aniAnimalList[index].scientific_name}</td>
+      </tr>
+    `;
+    table.innerHTML += row;
+    
+  }
+}
+
+aniBuildTable();
