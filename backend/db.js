200e8d5492d0cc6db5b97f1908daa8d5a2a802df
Animalsdev <ascendantspiral@gmail.com>
2026-02-26 14:36:43 +0000

    Hide db password under env file

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
+require('dotenv').config();
 
 const pool = new Pool({
   host: "127.0.0.1",
   port: 5432,
   user: "animals_user",
-  password: "Animals5user@",
+  password: process.env.DB_PASSWORD,
   database: "animalsdb",
 });
 
