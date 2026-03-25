a3aac15389e45637fdee168aedf9a9c4813ec082
Animalsdev <ascendantspiral@gmail.com>
2026-03-25 10:54:54 +0000

    Create gameboard with example pieces

--- frontend/game/game.js
+++ frontend/game/game.js
@@ -1,1 +1,22 @@
 const gameBoard = document.querySelector("#gameboard");
+const gamePiece = '<div class="game-piece"></div>'
+const startPieces = [gamePiece , gamePiece , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
+    , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
+    , '', '', '', '', '', '', '', ''
+    , '', '', '', '', '', '', '', ''
+    , '', '', '', '', '', '', '', ''
+    , '', '', '', '', '', '', '', ''
+    , gamePiece , gamePiece , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
+    , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
+];
+
+function createBoard() {
+    startPieces.forEach((startPiece) => {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        gameBoard.append(square);
+        square.innerHTML = startPiece;
+    })
+}
+
+createBoard();
