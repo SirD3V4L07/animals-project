493e7ca3c5e638de1e8ef38959f9ba895db19388
Animalsdev <ascendantspiral@gmail.com>
2026-04-02 16:26:49 +0100

    Add board alternating colors

--- frontend/game/game.js
+++ frontend/game/game.js
@@ -9,13 +9,33 @@ const startPieces = [gamePiece , gamePiece , gamePiece, gamePiece, gamePiece, ga
     , gamePiece , gamePiece , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
     , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
 ];
+let toggle = true;
 
 function createBoard() {
-    startPieces.forEach((startPiece) => {
+    startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div');
         square.classList.add('square');
         gameBoard.append(square);
         square.innerHTML = startPiece;
+
+        if (toggle) {
+            square.classList.add('even-square');
+        } else {
+            square.classList.add('odd-square');
+        }
+
+        //Toggle twice at the end of each row to fix alternating colors
+        if ((i == 7) ||
+            (i == 15) ||
+            (i == 23) ||
+            (i == 31) ||
+            (i == 39) ||
+            (i == 47) ||
+            (i == 55)) {
+            toggle = !toggle;
+        }
+        toggle = !toggle;
+        
     })
 }
 
