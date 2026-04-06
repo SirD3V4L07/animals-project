f01de771ba237abcfc4dcb1faa91b9e742cbaec0
Animalsdev <ascendantspiral@gmail.com>
2026-04-06 17:47:36 +0100

    Change gameboard array to multidimensional array

--- frontend/game/game.js
+++ frontend/game/game.js
@@ -1,42 +1,35 @@
 const gameBoard = document.querySelector("#gameboard");
 const gamePiece = '<div class="game-piece"></div>'
-const startPieces = [gamePiece , gamePiece , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
-    , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
-    , '', '', '', '', '', '', '', ''
-    , '', '', '', '', '', '', '', ''
-    , '', '', '', '', '', '', '', ''
-    , '', '', '', '', '', '', '', ''
-    , gamePiece , gamePiece , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
-    , gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece, gamePiece
+
+let boardArray = [
+    [gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece],
+    [gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    [gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece],
+    [gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece,gamePiece]
 ];
 let toggle = true;
 
 function createBoard() {
-    startPieces.forEach((startPiece, i) => {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        gameBoard.append(square);
-        square.innerHTML = startPiece;
-
-        if (toggle) {
-            square.classList.add('even-square');
-        } else {
-            square.classList.add('odd-square');
-        }
-
-        //Toggle twice at the end of each row to fix alternating colors
-        if ((i == 7) ||
-            (i == 15) ||
-            (i == 23) ||
-            (i == 31) ||
-            (i == 39) ||
-            (i == 47) ||
-            (i == 55)) {
+    boardArray.forEach((row, i) => {
+        boardArray[i].forEach((cell, j) => {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.innerHTML = cell;
+            gameBoard.append(square);
+            if (toggle) {
+                square.classList.add('even-square');
+            } else {
+                square.classList.add('odd-square');
+            }
             toggle = !toggle;
-        }
+        });
         toggle = !toggle;
-        
-    })
+    });
+    
 }
 
 createBoard();
