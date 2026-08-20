5801ec6fee0800a0fdc4050a8294789cb7b948aa
SirD3v4l07 <ascendantspiral@gmail.com>
2026-08-20 15:14:58 +0100

    Fetch piece background image

--- frontend/game/game.js
+++ frontend/game/game.js
@@ -1,3 +1,27 @@
+let boardArray = 
+[[createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top")],
+[createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top")],
+[null,null,null,null,null,null,null,null],
+[null,null,null,null,null,null,null,null],
+[null,null,null,null,null,null,null,null],
+[null,null,null,null,null,null,null,null],
+[createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom")],
+[createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom")]];
+
+let toggle = true;
+
+let selectedPiece = {
+    x: null,
+    y: null
+};
+
+let gameScore = {
+    top: 0,
+    bottom: 0
+};
+
+const gameBoard = document.querySelector("#gameboard");
+
 async function loadAnimal(id) {
     const response = await fetch(`/backend/data/${id}.json`);
 
@@ -15,8 +39,6 @@ async function assignAnimal(animal, x, y, player) {
     );
 }
 
-const gameBoard = document.querySelector("#gameboard");
-
 function createPiece(animal, player) {
     const piece = {
         animal: animal,
@@ -25,25 +47,6 @@ function createPiece(animal, player) {
 
     return piece;
 }
-
-let boardArray = 
-[[createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top")],
-[createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("","top"),createPiece("buteo","top")],
-[null,null,null,null,null,null,null,null],
-[null,null,null,null,null,null,null,null],
-[null,null,null,null,null,null,null,null],
-[null,null,null,null,null,null,null,null],
-[createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom")],
-[createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom"),createPiece("","bottom")]];
-let toggle = true;
-let selectedPiece = {
-    x: null,
-    y: null
-};
-let gameScore = {
-    top: 0,
-    bottom: 0
-};
 
 async function main() {
     await assignAnimal("buteo", 0, 1, "top");
@@ -57,7 +60,7 @@ function createBoard() {
     for (let i = 0; i < 8; i++) {
         
         for (let j = 0; j < 8; j++) {
-            
+            const currentSquare = boardArray[i][j];
             //Create squares and pieces
             const piece = document.createElement('div');
             piece.classList.add('piece');
@@ -75,14 +78,18 @@ function createBoard() {
             }
 
             //Append squares and piece elements to board element
-            if (boardArray[i][j] != null) {
-                boardArray[i][j].x = j;
-                boardArray[i][j].y = i;
-                if (boardArray[i][j].player === "top") {
+            
+            if (currentSquare != null) {
+                currentSquare.x = j;
+                currentSquare.y = i;
+                if (currentSquare.player === "top") {
                     piece.classList.add('top');
                 } else {
                     piece.classList.add('bottom');
                 }
+                if (currentSquare.animal) {
+                    piece.style.backgroundImage = `url(${currentSquare.animal.image})`;
+                }                
                 square.appendChild(piece);                
             }            
             gameBoard.append(square);
@@ -99,10 +106,6 @@ function clearHighlights() {
 function clearSelection() {
     selectedPiece.x = null;
     selectedPiece.y = null;
-}
-
-function refreshBoard() {
-
 }
 
 function addClickBehavior() {
